e10031f7a4024e24d0bf026433e37ea6d2845dd0
Mihir Singh <git.service@mihirsingh.com>
2016-03-06 08:17:49 -0500

    Created a view which posts the GPS coordinate changes

---
+++ Status/MapView.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  Status
+//
+//  Created by Mihir Singh on 3/5/16.
+//  Copyright © 2016 Mihir Singh. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapView: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet var latitudeLabel: UILabel!
+    @IBOutlet var longitudeLabel: UILabel!
+    @IBOutlet var mapView: MKMapView!
+    
+    var locationManager:CLLocationManager?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager = CLLocationManager()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager?.delegate = self;
+            locationManager?.desiredAccuracy = kCLLocationAccuracyBest;
+            locationManager?.distanceFilter = kCLDistanceFilterNone;
+            locationManager?.pausesLocationUpdatesAutomatically = false;
+            locationManager?.requestWhenInUseAuthorization();
+            locationManager?.requestAlwaysAuthorization();
+            locationManager?.allowsBackgroundLocationUpdates = true;
+            locationManager?.startUpdatingLocation();
+            
+        }
+    }
+    
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let latestLocation = locations.last!
+        
+        logLocation(latestLocation.coordinate.latitude, longitude: latestLocation.coordinate.longitude)
+    }
+    
+    func logLocation(latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+        latitudeLabel.text = latitude.description
+        longitudeLabel.text = longitude.description
+
+        var mapRegion = MKCoordinateRegion();
+        
+        mapRegion.center = mapView.userLocation.coordinate;
+        mapRegion.span.latitudeDelta = 0.005
+        mapRegion.span.longitudeDelta = 0.005
+        
+        mapView.setRegion(mapRegion, animated: true)
+        
+        let request = NSMutableURLRequest(URL: NSURL(string: "http://<server>/locations/")!)
+        request.HTTPMethod = "POST"
+        let postString = "lat=\(latitude)&long=\(longitude)"
+        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { data, response, error in
+            guard error == nil && data != nil else {                                                          // check for fundamental networking error
+                print("error=\(error)")
+                return
+            }
+            
+            if let httpStatus = response as? NSHTTPURLResponse where httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response)")
+            }
+            
+            let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+            print("responseString = \(responseString)")
+        }
+        task.resume()
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
